b19a4042f5dafe8db7d5342499c5f777c355d831
jinxian.gong <jinxian.gong@aispeech.com>
2022-12-16 10:54:42 +0800

    init

---
+++ app/src/main/java/com/xd/spring2/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.xd.spring2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ *   代码优化工具箱
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
